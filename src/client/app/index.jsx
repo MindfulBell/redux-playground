6baa2a8b8b89491d88268a2b7adcb7e1dc92012f
MindfulBell <tim.bell02@gmail.com>
2016-02-09 21:27:30 -0800

    small change

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello TITTIES!</p>;
+    return <p> Hello!</p>;
   }
 }
 
@@ -12,3 +12,5 @@ render(<App/>, document.getElementById('app'));
 //commands to know:
 //npm run dev --> turns on webpack watch for updating changes
 //npm run build --> runs webpack in dev mode
+
+//DON'T FORGET TO CREATE A NEW GIT HUB REPO TO PUSH TO OR IT WILL OVERRIDE THE TEMPLATE!!
