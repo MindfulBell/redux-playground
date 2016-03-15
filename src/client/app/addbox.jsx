1d5872ea976ddbefb18e3d10e5db5b0621984053
Joseph Morse <tamed.lionheart@gmail.com>
2016-03-15 02:41:57 -0400

    Fixed the edit recipe button
    Did just enough to enable the edit button to work properly.  Partly had
    to make sure that there was a id being passed to work with, and changed
    it to use nextProps in componentWillMount.

--- src/client/app/addbox.jsx
+++ src/client/app/addbox.jsx
@@ -1,116 +1,94 @@
-import React, { Component } from 'react';
-import Bootstrap from 'bootstrap';
-
-
-//ADD vs EDIT text on the pop up box...
-//use recipeToEdit props here somewhere, not in state necessarily, but down in the value?
+import React, {Component} from 'react';
+import 'bootstrap';
 
 class AddBox extends Component {
-	constructor(props) {		
-		super(props)		
-		this.state = {
-			recipeVal: '',
-			ingredientsVal: '',
-			instructionsVal: ''
-		};
-	}
+  constructor(props) {
+    super(props);
 
-	componentWillReceiveProps(){
-		this.setState({
-			recipeVal: this.props.recipeToEdit.recipeName,
-			ingredientsVal: this.props.recipeToEdit.ingredients,
-			instructionsVal: this.props.recipeToEdit.instructions
-		})
-	}
+    this.state = {
+      recipeVal: '',
+      ingredientsVal: '',
+      instructionsVal: ''
+    };
+  }
 
-	handleChange(val){
-	  if (val.id === 'recipeInput') {
-	    this.setState({
-	      recipeVal: val.value
-	    });
-	  }
-	  else if (val.id === 'ingredientInput') {
-	    this.setState({
-	      ingredientsVal: val.value
-	    });
-	  }
-	  else {
-	    this.setState({
-	      instructionsVal: val.value
-	    });
-	  }
-	}
-	
-	handleSubmit(event) {
-	  event.preventDefault();
-	  if (this.state.ingredientsVal !== '' && this.state.instructionsVal !== '' && this.state.recipeVal !== '') {
-		  const ingredientsArr = this.state.ingredientsVal.split(',');
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      recipeVal: nextProps.recipeToEdit.recipe.recipeName,
+      ingredientsVal: nextProps.recipeToEdit.recipe.ingredients,
+      instructionsVal: nextProps.recipeToEdit.recipe.instructions
+    });
+  }
 
-		  //send to recipehandler
-		  this.props.handleRecipe(null, event.target.id, this.state.recipeVal, ingredientsArr, this.state.instructionsVal)
-		}
-		//clear the add recipe form
-	  this.setState({
-		  recipeVal: '',
-		  ingredientsVal: '',
-		  instructionsVal: ''
-		})
-	}
-	
-	handleClose(){
-		this.setState({
-			recipeVal: '',
-			ingredientsVal: '',
-			instructionsVal: ''
-		})
-	}
+  handleChange(val) {
+    if (val.id === 'recipeInput') {
+      this.setState({recipeVal: val.value});
+    }
 
-  render () {
-  	console.log('Recipe Name To Edit: ' + this.state.recipeVal)
+    else if (val.id === 'ingredientInput') {
+      this.setState({ingredientsVal: val.value});
+    }
 
-    return  (
+    else {
+      this.setState({instructionsVal: val.value});
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.ingredientsVal !== '' && this.state.instructionsVal !== '' && this.state.recipeVal !== '') {
+      const ingredientsArr = this.state.ingredientsVal.split(',');
+
+      //send to recipehandler
+      this.props.handleRecipe(this.props.recipeToEdit.id, event.target.id, this.state.recipeVal, ingredientsArr, this.state.instructionsVal)
+    }
+    //clear the add recipe form
+    this.setState({recipeVal: '', ingredientsVal: '', instructionsVal: ''})
+  }
+
+  handleClose() {
+    this.setState({recipeVal: '', ingredientsVal: '', instructionsVal: ''})
+  }
+
+  render() {
+    console.log('Recipe Name To Edit: ' + this.state.recipeVal)
+
+    return (
       <div className='modal fade' tabIndex='-1' role='dialog' id='addPopUp' aria-labelledby="recipetitle" aria-hidden='true'>
-      	<div className='modal-dialog'>      	
-	      	<div className='modal-content'>
-	      		<div className='modal-header'>
-	      			<button onClick={event => this.handleClose()} type="button" 
-	      			className="close" data-dismiss="modal">
-	      			<span aria-hidden="true">&times;</span>
-	      			</button>
-	        		<h4 className='modal-title' id='recipetitle'>
-	        		Add Your Recipe!
-	        		</h4>
-	      		</div>
-	      		<div className='modal-body'>
-			        <form role='form'>
-			          <div className='form-group' id='recipeAdd'> 
-			            <label htmlFor='recipeName'>Recipe Name</label>
-			            <input type="recipe" className='form-control' id="recipeInput"
-			            placeholder='Name your recipe!' value={this.state.recipeVal} 
-			            onChange={event => this.handleChange(event.target)}/>
-			          </div>
-			          <div className='form-group' id='ingredientAdd'>
-			            <label htmlFor='ingredientList'>Ingredients</label>
-			            <textarea type='ingredients' className='form-control' id='ingredientInput' rows='3' max-width='250px' 
-			            placeholder='List your ingredients separated by commas!' value={this.state.ingredientsVal}
-			            onChange={event => this.handleChange(event.target)}></textarea>
-			          </div>
-			          <div className='form-group' id='instructionAdd'>
-			            <label htmlFor='instructionList'>Instructions</label>
-			            <textarea type='instructions' className='form-control' id='instructionInput' rows='3' max-width='250px' 
-			            placeholder='Write out your instructions!' value={this.state.instructionsVal} 
-			            onChange={event => this.handleChange(event.target)}></textarea>
-			          </div>
-			          <button type='submit' id='addButton' className='btn btn-primary' onClick={this.handleSubmit.bind(this)}>Save</button>			          
-			        </form>
-	      		</div>
-	      		<div className='modal-footer'>
-	      			<button onClick={event => this.handleClose()} type='button' className='btn btn-danger' data-dismiss='modal' >Close</button>
-			    </div>
-	      	</div>
-	    </div>	    
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <button onClick={event => this.handleClose()} type="button" className="close" data-dismiss="modal">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <h4 className='modal-title' id='recipetitle'>
+                Add Your Recipe!
+              </h4>
+            </div>
+            <div className='modal-body'>
+              <form role='form'>
+                <div className='form-group' id='recipeAdd'>
+                  <label htmlFor='recipeName'>Recipe Name</label>
+                  <input type="recipe" className='form-control' id="recipeInput" placeholder='Name your recipe!' value={this.state.recipeVal} onChange={event => this.handleChange(event.target)}/>
+                </div>
+                <div className='form-group' id='ingredientAdd'>
+                  <label htmlFor='ingredientList'>Ingredients</label>
+                  <textarea type='ingredients' className='form-control' id='ingredientInput' rows='3' max-width='250px' placeholder='List your ingredients separated by commas!' value={this.state.ingredientsVal} onChange={event => this.handleChange(event.target)}></textarea>
+                </div>
+                <div className='form-group' id='instructionAdd'>
+                  <label htmlFor='instructionList'>Instructions</label>
+                  <textarea type='instructions' className='form-control' id='instructionInput' rows='3' max-width='250px' placeholder='Write out your instructions!' value={this.state.instructionsVal} onChange={event => this.handleChange(event.target)}></textarea>
+                </div>
+                <button type='submit' id='addButton' data-dismiss='modal' className='btn btn-primary' onClick={this.handleSubmit.bind(this)}>Save</button>
+              </form>
+            </div>
+            <div className='modal-footer'>
+              <button onClick={event => this.handleClose()} type='button' className='btn btn-danger' data-dismiss='modal'>Close</button>
+            </div>
+          </div>
+        </div>
       </div>
-    	)  	
+    )
   }
 }
 
